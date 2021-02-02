cd12de9c2ae13e9ecb0f3bf60489592443ff29fe
Will McQuiston <70789928+willmcq57@users.noreply.github.com>
2021-02-02 03:04:00 -0500

    setup

---
+++ README.md
@@ -0,0 +1,7 @@
+SETUP
+
+Set environment variables to contain username/password with the following commands
+
+set robinhood_username=your_username_here
+set robinhood_password=your_password_here
+
--- options.py
+++ options.py
@@ -1,6 +1,13 @@
 import robin_stocks as rs
+import os
 
-rs.login(username=your_username,
-         password=your_password,
+robin_user = os.environ.get("robinhood_username")
+robin_pass = os.environ.get("robinhood_password")
+
+rs.login(username=robin_user,
+         password=robin_pass,
          expiresIn=86400,
          by_sms=True)
+
+
+rs.export_completed_option_orders(".","options")
